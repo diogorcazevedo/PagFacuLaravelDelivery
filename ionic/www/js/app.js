4bdda9cf4a96df239dfac3c7205382366a55601d
Diogo Azevedo <diogorcazevedo@gmail.com>
2015-11-14 11:58:00 -0200

    Corrigindo o Terceiro Commit Ionic via PHP STORM

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,39 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider,$urlRouterProvider){
+        //
+        // For any unmatched url, redirect to /state1
+        $urlRouterProvider.otherwise("/");
+
+        //
+        // Now set up the states
+        $stateProvider
+
+            .state('home',{
+                url:'/home/:nome',
+                templateUrl:"templates/home.html",
+                controller: 'HomeCtrl'
+            })
+            .state('home.a',{
+                url:'/a',
+                templateUrl:"templates/home-a.html"
+            })
+            .state('home.b',{
+                url:'/b',
+                templateUrl:"templates/home-b.html"
+            })
+            .state('main',{
+                url:'/main',
+                templateUrl:"templates/main.html"
+            })
+            .state('main.a',{
+                url:'/a',
+                templateUrl:"templates/main-a.html"
+            })
+            .state('main.b',{
+                url:'/b',
+                templateUrl:"templates/main-b.html"
+            })
+});
